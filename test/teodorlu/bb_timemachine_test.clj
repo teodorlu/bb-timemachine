7a99aecb880695d02f08ddc2386c4b35676075a6
Teodor Heggelund <git@teod.eu>
2025-12-21 14:35:03 +0100

    Now that we have a commit, we may test it

---
+++ test/teodorlu/bb_timemachine_test.clj
@@ -0,0 +1,25 @@
+(ns teodorlu.bb-timemachine-test
+  (:require [babashka.fs :as fs]
+            [clojure.test :refer [deftest is testing]]
+            [teodorlu.bb-timemachine :as timemachine]))
+
+(def initial-commit-sha "2bce149ff003d8c372db63717459ca8c3d241508")
+
+(deftest worktree-add-remove
+  (let [tempdir (fs/create-temp-dir)
+        worktree-dir (str (fs/file tempdir initial-commit-sha))
+        worktree-list-dir #(when (fs/exists? worktree-dir)
+                             (fs/list-dir worktree-dir))]
+    (testing "At first, there are no files in the worktree folder"
+      (is (empty? (worktree-list-dir))))
+
+    (testing "After worktree-add, we can find our README in the worktree folder"
+      (timemachine/worktree-add "." worktree-dir initial-commit-sha)
+      (try
+        (is (contains? (set (map fs/file-name (worktree-list-dir)))
+                       "deps.edn"))
+        (finally
+          (timemachine/worktree-remove "." worktree-dir))))
+
+    (testing "After worktree-remove, the folder is empty."
+      (is (empty? (worktree-list-dir))))))
