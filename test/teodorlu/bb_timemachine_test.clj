edcf48a2b10b6d571242ef60fbd61b76d11ec51c
Teodor Heggelund <git@teod.eu>
2025-12-21 22:32:13 +0100

    Add test for timemachine/do-at

--- test/teodorlu/bb_timemachine_test.clj
+++ test/teodorlu/bb_timemachine_test.clj
@@ -5,18 +5,18 @@
 
 (set! *warn-on-reflection* true)
 
-(def initial-commit-sha "2bce149ff003d8c372db63717459ca8c3d241508")
+(def beginning-of-time "2bce149ff003d8c372db63717459ca8c3d241508")
 
 (deftest worktree-add-remove
   (let [tempdir (fs/create-temp-dir)
-        worktree-dir (str (fs/file tempdir initial-commit-sha))
+        worktree-dir (str (fs/file tempdir beginning-of-time))
         worktree-list-dir #(when (fs/exists? worktree-dir)
                              (fs/list-dir worktree-dir))]
     (testing "At first, there are no files in the worktree folder"
       (is (empty? (worktree-list-dir))))
 
     (testing "After worktree-add, we can find our README in the worktree folder"
-      (timemachine/worktree-add "." worktree-dir initial-commit-sha)
+      (timemachine/worktree-add "." worktree-dir beginning-of-time)
       (try
         (is (contains? (set (map fs/file-name (worktree-list-dir)))
                        "deps.edn"))
@@ -25,3 +25,13 @@
 
     (testing "After worktree-remove, the folder is empty."
       (is (empty? (worktree-list-dir))))))
+
+(deftest do-at
+  (is (= (timemachine/do-at beginning-of-time (constantly ::result))
+         ::result))
+  (is (contains? (timemachine/do-at beginning-of-time
+                   (fn [dir]
+                     (->> (fs/list-dir dir)
+                          (map fs/file-name)
+                          set)))
+                 "deps.edn")))
